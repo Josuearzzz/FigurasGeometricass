518ad501f342f86544ed871306e0add165433a8f
Josue arias <josuearias0218@gmail.com>
2026-06-09 16:45:21 -0600

    Clase circulo
    se crea la clas circulo que hereda de FigGeo y define sus atributos especificos

--- src/circulos/Circulo.java
+++ src/circulos/Circulo.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package circulos;
 
-/**
- *
- * @author Cristopher Arias
- */
-public class Circulo {
+import figurasgeometrica.FigGeo;
+
+public class Circulo extends FigGeo {
+    private double radio;
+    private static final double PI=Math.PI;
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public double getDiametro(){
+       return radio*2; 
+        
+    }
+    public static double getPI() {
+        return PI;
+    }
     
+    public void setRadio(double radio){
+        this.radio = radio;
+    }
+    
+    public Circulo(double radio){
+        this.radio=radio;
+    }
+    
+    @Override
+    public double calcularPerimetro(){
+        this.perimetro=2*radio*PI;
+        return perimetro;
+    }
+   
+    
+    @Override
+     public double calcularArea(){
+        this.area=Math.pow(radio, 2)*PI;
+        return area;
+    }
+              
 }
