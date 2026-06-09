53c9a2905e3f1fdf6523ef0f6c816014eedb259a
Josuearzzz <josuearias0218@gmail.com>
2026-06-09 15:59:37 -0600

    Clase circulo
    se agrega clase circulo

---
+++ src/figurasgeometrica/FigGeo.java
@@ -0,0 +1,32 @@
+
+package figurasgeometrica;
+
+
+public abstract class FigGeo {
+    private double perimetro;
+    private double area;
+
+    public double getPerimetro() {
+        return perimetro;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+public abstract double calcularPerimetro();
+ 
+public abstract double calcularArea();  
+
+
+
+
+
+
+
+
+
+
+
+
+}
